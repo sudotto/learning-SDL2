dec34a0840e81bc50070269fb717c20b9216c9e2
sudotto <codekiwi132@gmail.com>
2024-09-21 13:57:17 -0700

    window setup

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(){
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow("SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+
+	SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+	SDL_Event event;
+
+	int running = 1;
+	while(running){
+		SDL_PumpEvents();
+		while(SDL_PollEvent(&event) != 0){
+			if(event.type == SDL_QUIT){
+				running = 0;
+			}
+		}
+		SDL_UpdateWindowSurface(window);
+	}
+	SDL_Quit();
+	return 0;
+}
